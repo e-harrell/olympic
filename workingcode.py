8de9d29161098d7901681b719861ca2fbf9c383a
e-harrell <E_A_harrell@hotmail.com>
2024-08-15 11:10:20 -0400

    working code for olympic medal table

---
+++ workingcode.py
@@ -0,0 +1,16 @@
+import requests 
+import pandas as pd
+from bs4 import BeautifulSoup
+from io import StringIO
+
+#url = StringIO("https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table")
+url = "https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+tables = soup.find_all("table")
+i=0
+
+df = pd.read_html(StringIO(str(tables[1])))
+print(df)
+print(type(df))
+print(len(df))
